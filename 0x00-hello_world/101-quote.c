a700307e82a2ab72c65fdd7f425f1eb988060753
TeekayCarter <TeekayCarter@users.noreply.github.com>
2022-10-14 07:15:24 -0700

    Fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,11 @@
-#include <stdio.h>
-#include <string.h>
+#include<stdio.h>
 #include <unistd.h>
 /**
- * main - main block
+ * main - Function to print text using putchar
  * Return: 1
  */
 int main(void)
 {
-	int s = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
-
-	write(2,
-	      "and that piece of art is useful - Dora Korpar, 2015-10-19\n",
-	      s);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
